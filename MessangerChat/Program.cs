0b82075b9acdcb28695d9f91cb5334be1e025645
Ismoil Tohirov <ismoiltohirov55@gmail.com>
2024-02-13 09:30:12 +0500

    Add project files.

---
+++ MessangerChat/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MessangerChat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User :)");
+            Console.WriteLine("First, Enter your Username: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("And now Enter your Password: ");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("1) Users List");
+            Console.WriteLine("2) Messages");
+            string number = Console.ReadLine();
+
+            int num = Int32.Parse(number);
+
+            switch (num)
+            {
+                case 1:
+                    UI.UsersList();
+                    string ToUser = Console.ReadLine();
+
+                    break;
+            }
+        }
+    }
+}
---
+++ MessangerChat/UI.cs
@@ -0,0 +1,36 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessangerChat
+{
+    public class UI
+    {
+        public static void UsersList()
+        {
+            string connectionString = "Host=localhost;Port=5432;Database=connected;username=postgres;Password=psqlDB;";
+            
+            using(NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @$"SELECT * FROM Users;";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query,connection);
+
+                var result = cmd.ExecuteReader();
+
+                while (result.Read())
+                {
+                    Console.WriteLine(result[1]);
+                }
+
+                Console.WriteLine("Choose username: ");
+
+                connection.Close();
+            }
+        }
+    }
+}
